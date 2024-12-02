0404f9697927b2f8f6aa016f0d37c17ee7b855d2
Snow.Z <ideadsnow@gmail.com>
2024-12-02 16:36:33 +0800

    update favicons

---
+++ script/favicon/process_favicon.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 输入文件
+input_file="favicon.svg"
+
+# 转换为 ICO
+magick -density 300 "$input_file" -resize 48x48 -define icon:auto-resize=48 favicon.ico
+
+# 生成不同大小的 PNG
+for size in 16 32 64 96 128 192 256 512; do
+    magick -density 300 "$input_file" -resize ${size}x${size} "favicon-${size}x${size}.png"
+done
+
+# mv favicon-128x128.png apple-touch-icon.png
+# mv favicon-192x192.png web-app-manifest-192x192.png
+# mv favicon-512x512.png web-app-manifest-512x512.png
