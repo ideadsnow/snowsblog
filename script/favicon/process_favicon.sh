4bdbcb8e82ad8a2d6f4e46f333c99b7023850542
Snow.Z <ideadsnow@gmail.com>
2024-12-03 18:57:03 +0800

    commit

--- script/favicon/process_favicon.sh
+++ script/favicon/process_favicon.sh
@@ -7,7 +7,7 @@ input_file="favicon.svg"
 magick -density 300 "$input_file" -resize 48x48 -define icon:auto-resize=48 favicon.ico
 
 # 生成不同大小的 PNG
-for size in 16 32 64 96 128 192 256 512; do
+for size in 16 32 48 64 96 128 192 256 512; do
     magick -density 300 "$input_file" -resize ${size}x${size} "favicon-${size}x${size}.png"
 done
 
